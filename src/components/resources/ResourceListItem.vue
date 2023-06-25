12abb694abe19f4650f6722f980ba5b419364d0a
Ahmadreza Ahmadi <ahmadrezaahmadi.dev@gmail.com>
2023-06-25 10:41:15 +0330

    style: improve styles in ResourceListItem component

--- src/components/resources/ResourceListItem.vue
+++ src/components/resources/ResourceListItem.vue
@@ -11,13 +11,22 @@ export default {
 
 <template>
   <li>
-    <header>
-      <h3>{{ resource.title }}</h3>
-      <button>Delete</button>
+    <header class="flex items-center justify-between">
+      <h3 class="text-2xl font-bold my-2">{{ resource.title }}</h3>
+      <button
+        class="bg-gray-50 text-red-500 text-sm px-2 py-1 rounded-md border border-gray-200"
+      >
+        Delete
+      </button>
     </header>
-    <p>{{ resource.description }}</p>
+    <p class="font-medium text-lg my-2">{{ resource.description }}</p>
     <nav>
-      <a :href="resource.link" target="_blank">View Resource</a>
+      <a
+        :href="resource.link"
+        target="_blank"
+        class="font-medium text-orange-600 hover:text-orange-400 active:text-orange-400"
+        >View Resource</a
+      >
     </nav>
   </li>
 </template>
