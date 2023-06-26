4c4b83ce2ed95083783d7c0b0ab344e19a8a12ce
Ahmadreza Ahmadi <ahmadrezaahmadi.dev@gmail.com>
2023-06-26 10:27:10 +0330

    refactor(form-validation): teleport error dialog into body tag

--- src/components/base/BaseDialog.vue
+++ src/components/base/BaseDialog.vue
@@ -12,21 +12,23 @@ export default {
 </script>
 
 <template>
-  <div class="fixed top-0 left-0 w-full h-screen z-10 bg-black/75" @click="$emit('close')"></div>
-  <dialog open class="fixed mx-auto p-0 w-[32rem] top-1/2 -translate-y-1/2 z-10 rounded-2xl">
-    <header class="p-4">
-      <slot name="header">
-        <h2>{{ title }}</h2>
-      </slot>
-    </header>
-    <hr />
-    <section class="p-4">
-      <slot name="body"> </slot>
-    </section>
-    <menu class="flex justify-end p-4">
-      <slot name="actions">
-        <BaseButton size="lg" @click="$emit('close')">Close</BaseButton>
-      </slot>
-    </menu>
-  </dialog>
+  <Teleport to="body">
+    <div class="fixed top-0 left-0 w-full h-screen z-10 bg-black/75" @click="$emit('close')"></div>
+    <dialog open class="fixed mx-auto p-0 w-[32rem] top-1/2 -translate-y-1/2 z-10 rounded-2xl">
+      <header class="p-4">
+        <slot name="header">
+          <h2>{{ title }}</h2>
+        </slot>
+      </header>
+      <hr />
+      <section class="p-4">
+        <slot name="body"> </slot>
+      </section>
+      <menu class="flex justify-end p-4">
+        <slot name="actions">
+          <BaseButton size="lg" @click="$emit('close')">Close</BaseButton>
+        </slot>
+      </menu>
+    </dialog>
+  </Teleport>
 </template>
